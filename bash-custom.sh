762e1e78e9129a31fd6da11564d6d67eb695b38f
lucduke <42948608+lucduke@users.noreply.github.com>
2021-04-05 22:35:48 +0200

    Update bash-custom.sh

--- bash-custom.sh
+++ bash-custom.sh
@@ -21,17 +21,25 @@ echo "- Sauvegarder la configuration bash de l'utilisateur courant"
 cd ~
 cp .bashrc bashrc-$timestamp.bak
 
-echo "- Personnaliser la commande ls"
+echo "- Personnaliser la commande ls 1/3"
 if grep -Fq "# export LS_OPTIONS" .bashrc
   then
-    echo "- LS option color commentée"
+    echo "- LS option color commentée 1/3"
     sed -i.bak 's/# export LS_OPTION/export LS_OPTION/' .bashrc
   else
-    echo "- LS option color déja non commentée"
+    echo "- LS option color déja non commentée 1/3"
+fi
+
+echo "- Personnaliser la commande ls 2/3"
+if grep -Fq "# eval \"\`dircolors\`\"" .bashrc
+  then
+    echo "- LS option color commentée 2/3"
+    sed -i.bak 's/# eval \"\`dircolors\`\"/eval \"\`dircolors\`\"/' .bashrc
+  else
+    echo "- LS option color déja non commentée 2/3"
 fi
 
 # You may uncomment the following lines if you want `ls' to be colorized:
-# export LS_OPTIONS='--color=auto'
 # eval "`dircolors`"
 # alias ls='ls $LS_OPTIONS'
 
